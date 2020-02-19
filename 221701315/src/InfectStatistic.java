a38f7083d8b167f701d3e0e9e6abb4aa72e892ae
mitwu <60768482+mitwu@users.noreply.github.com>
2020-02-19 14:17:02 +0800

    Update InfectStatistic.java

--- 221701315/src/InfectStatistic.java
+++ 221701315/src/InfectStatistic.java
@@ -14,7 +14,7 @@ public class InfectStatistic
 		"台湾","天津","西藏","香港","新疆","云南","浙江"
 	};
 	static int[][] people_count=new int[35][4];//定义一个二维数组记录所有种类的人数依次为感染 疑似 治愈 死亡
-	public static void main(String [] args)
+	public static void main(String [] args)//处理传入的参数
 	{
 		for(int i=0;i<args.length;i++)
 		{
@@ -117,7 +117,7 @@ public class InfectStatistic
 	}
 	public void read_text(String path) 
 	{
-		people_count=first_give();
+		people_count=first_give();//将所有省市的的人数初始化
 		try 
 		{
 			File afile=new File(path);
@@ -129,16 +129,77 @@ public class InfectStatistic
 	            String line_txt = null;
 	            while((line_txt=buffered_reader.readLine())!=null)
 	            {
-	            	if(line_txt.contains("//"))
+	            	if(line_txt.contains("//"))//文档结尾，统计全国人数
 	            	{
+	            		for(int i=1;i<province1.length;i++)
+	            		{
+	            			for(int j=0;j<4;j++)
+	            			{
+	            				people_count[0][j]=people_count[0][j]+people_count[i][j];
+	            			}
+	            		}
 	            		break;
 	            	}
 	            	String[] split=line_txt.split(" ");//分割读进来的字符串
 	            	for(int i=0;i<province1.length;i++)
 	            	{
-	            		if(split[0]==province1[i])
+	            		if(split[0]==province1[i])//判断省份
 	            		{
-	            			
+	            			if(split[1]=="新增")
+	            			{
+	            				if(split[2]=="感染患者")
+	            				{
+	            					people_count[i][0]=people_count[i][0]+Integer.parseInt(split[3].substring(0,split[3].length()-1));
+	            				}
+	            				else if(split[2]=="疑似患者")
+	            				{
+	            					people_count[i][1]=people_count[i][1]+Integer.parseInt(split[3].substring(0,split[3].length()-1));
+	            				}
+	            			}
+	            			else if(split[1]=="死亡")
+	            			{
+	            				people_count[i][3]=people_count[i][3]+Integer.parseInt(split[2].substring(0,split[2].length()-1));
+	            				people_count[i][0]=people_count[i][0]-Integer.parseInt(split[2].substring(0,split[2].length()-1));
+	            			}
+	            			else if(split[1]=="治愈")
+	            			{
+	            				people_count[i][2]=people_count[i][2]+Integer.parseInt(split[2].substring(0,split[2].length()-1));
+	            				people_count[i][0]=people_count[i][0]-Integer.parseInt(split[2].substring(0,split[2].length()-1));
+	            			}
+	            			else if(split[1]=="排除")
+	            			{
+	            				people_count[i][1]=people_count[i][1]-Integer.parseInt(split[3].substring(0,split[3].length()-1));
+	            			}
+	            			else if(split[1]=="疑似患者")
+	            			{
+	            				if(split[2]=="流入")
+	            				{
+	            					for(int j=0;j<province1.length;j++)
+	            					{
+	            						if(split[3]==province1[j])
+	            						{
+	            							people_count[i][1]=people_count[i][1]-Integer.parseInt(split[4].substring(0,split[4].length()-1));
+	            							people_count[j][1]=people_count[j][1]+Integer.parseInt(split[4].substring(0,split[4].length()-1));
+	            						}
+	            					}
+	            				}
+	            				else if(split[2]=="确诊感染")
+	            				{
+	            					people_count[i][1]=people_count[i][1]-Integer.parseInt(split[3].substring(0,split[3].length()-1));
+	            					people_count[i][0]=people_count[i][0]+Integer.parseInt(split[3].substring(0,split[3].length()-1));
+	            				}
+	            			}
+	            			else if(split[1]=="感染患者")
+	            			{
+	            				for(int j=0;j<province1.length;j++)
+            					{
+            						if(split[3]==province1[j])
+            						{
+            							people_count[i][0]=people_count[i][0]-Integer.parseInt(split[4].substring(0,split[4].length()-1));
+            							people_count[j][0]=people_count[j][0]+Integer.parseInt(split[4].substring(0,split[4].length()-1));
+            						}
+            					}
+	            			}
 	            		}
 	            	}
 	            }
@@ -151,4 +212,8 @@ public class InfectStatistic
 		}
 	}
 	
+	public void write_txt(String path)//用来将数据输出到txt文件中
+	{
+		
+	}
 }

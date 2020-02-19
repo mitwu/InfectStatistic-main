da2cd1c60f8e3524d984d4cbb7209135cdccb7cb
mitwu <60768482+mitwu@users.noreply.github.com>
2020-02-19 12:43:27 +0800

    Update InfectStatistic.java

--- 221701315/src/InfectStatistic.java
+++ 221701315/src/InfectStatistic.java
@@ -13,8 +13,7 @@ public class InfectStatistic
 		 "宁夏","青海","山东","山西","陕西","上海","四川",
 		"台湾","天津","西藏","香港","新疆","云南","浙江"
 	};
-	
-	
+	static int[][] people_count=new int[35][4];//定义一个二维数组记录所有种类的人数依次为感染 疑似 治愈 死亡
 	public static void main(String [] args)
 	{
 		for(int i=0;i<args.length;i++)
@@ -101,6 +100,54 @@ public class InfectStatistic
 					}
 				}
 			}
+		}
+	}
+	
+	public int[][] first_give()//初始化
+	{
+		int[][] a=new int[35][4];
+		for(int i=0;i<35;i++)
+		{
+			for(int j=0;j<4;j++)
+			{
+				a[i][j]=0;
+			}
+		}
+		return a;
+	}
+	public void read_text(String path) 
+	{
+		people_count=first_give();
+		try 
+		{
+			File afile=new File(path);
+			if(afile.isFile() && afile.exists())
+			{
+				InputStreamReader read = new InputStreamReader(
+	                    new FileInputStream(afile),"UTF-8");
+	            BufferedReader buffered_reader = new BufferedReader(read);
+	            String line_txt = null;
+	            while((line_txt=buffered_reader.readLine())!=null)
+	            {
+	            	if(line_txt.contains("//"))
+	            	{
+	            		break;
+	            	}
+	            	String[] split=line_txt.split(" ");//分割读进来的字符串
+	            	for(int i=0;i<province1.length;i++)
+	            	{
+	            		if(split[0]==province1[i])
+	            		{
+	            			
+	            		}
+	            	}
+	            }
+	            read.close();
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.print("读取文件出错");
 		}
 	}
 	
